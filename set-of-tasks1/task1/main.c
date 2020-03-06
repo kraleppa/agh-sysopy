d1d162d16b84ea8a71eeaf6c5a2ece88840bfefa
Citoslav <misiek.nalepa@wp.pl>
2020-03-06 17:17:03 +0100

    idk what is happening :(((((

--- set-of-tasks1/task1/main.c
+++ set-of-tasks1/task1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
+#include <ctype.h>
 
 //connects two strings
 char *concat(const char *s1, const char *s2)
@@ -17,7 +18,43 @@ void compareTwoFiles(char *fileName1, char *fileName2){
     system(command);
 }
 
+int getNumberOfOperations(){
+    FILE *filePoiner = fopen("tmp.txt", "r");
+    if (filePoiner == NULL){
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[255];
+    int numberOfOperations = 0;
+
+    while (fgets(buffer, 255, filePoiner)){
+        if (isdigit(buffer[0])){
+            numberOfOperations++;
+        }  
+    }
+    return numberOfOperations;
+}
+
+void initializeBlockOfEditingOperations(){
+    FILE *filePoiner = fopen("tmp.txt", "r");
+    char buffer[255];
+    if (filePoiner == NULL){
+        exit(EXIT_FAILURE);
+    }
+    int numberOfOperations = getNumberOfOperations();
+    printf("AAAAAa");
+    char **block = calloc(numberOfOperations, sizeof(char*));
+    
+    int i = 0;
+    while (fgets(buffer, 255, filePoiner)){
+        if (isdigit(buffer[0])){
+            block[i] = "eluwina";
+            i++;
+        }  
+    }
+}
+
 
 int main(){
-    compareTwoFiles("a.txt", "b.txt");
+    initializeBlockOfEditingOperations();
 }
