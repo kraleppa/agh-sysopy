9fe296c8dfc623dc02329c4524fd1e971d596edc
Citoslav <misiek.nalepa@wp.pl>
2020-03-06 14:44:38 +0100

    changes

--- Tutorial/compiling multiple files/function1.c
+++ Tutorial/compiling multiple files/function1.c
@@ -1,5 +1,3 @@
-//in c files you keep implementations of functions which you declared in .h file
-//you have to include .h file simply by writing:
 #include "functions.h"
 
 int squareRoot(int number){
---
+++ Tutorial/libraries/static/function2.c
@@ -0,0 +1,6 @@
+#include "functions.h"
+#include <stdio.h>
+
+void start(){
+    printf("Start to execute program\n");
+}
