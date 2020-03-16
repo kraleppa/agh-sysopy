8ede308efa6ef69d82de45c3eb5318a3ccb65617
Citoslav <misiek.nalepa@wp.pl>
2020-03-16 14:25:07 +0100

    sorting with system commands

--- set-of-tasks2/task1/main.c
+++ set-of-tasks2/task1/main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 //connect two strings
 char *concat(const char *s1, const char *s2)
@@ -31,9 +35,75 @@ void generateWords(int number, int length){
         char *command = concat("echo ", concat(word, " >> dane.txt"));
         system(command);
     }
-
 }
+
+//return bigger string in lexicographical order.
+// 0 -> equals, 1 -> string1 < string2, -1 -> string1 > string2
+int compareStrings(char *string1, char *string2){
+    int length = strlen(string1);
+    for (int i = 0; i < length; i++){
+        if (string1[i] > string2[i]){
+            return -1;
+        }
+        if (string1[i] < string2[i]){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+//return 'index'-th record from file.
+char *getRecord(int fileDescriptor, int index, int lengthOfRecord){
+    char * block = calloc(lengthOfRecord, sizeof(char));
+    lseek(fileDescriptor, (lengthOfRecord + 1) * index, 0);
+    read(fileDescriptor, block, lengthOfRecord);
+    return block;
+}
+
+//save record on 'index'-th position in file
+void saveRecord(int fileDescriptor, char *record, int index, int lengthOfRecord){
+    lseek(fileDescriptor, (lengthOfRecord + 1) * index, 0);
+    write(fileDescriptor, record, lengthOfRecord);
+}
+
+//swap record on index1 with record in index2 in file
+void swapInFile(int fileDescriptor, int index1, int index2, int lengthOfRecord){
+    char *tmp = getRecord(fileDescriptor, index1, lengthOfRecord);
+    saveRecord(fileDescriptor, getRecord(fileDescriptor, index2, lengthOfRecord), index1, lengthOfRecord);
+    saveRecord(fileDescriptor, tmp, index2, lengthOfRecord);
+}
+
+//partition with system commands
+int partition(int fileDescriptor, int lengthOfRecord, int left, int right){
+    char *pivot = getRecord(fileDescriptor, right, lengthOfRecord);
+    int i = left - 1;
+    for (int j = left; j < right; j++){
+        char *record = getRecord(fileDescriptor, j, lengthOfRecord);
+        if (compareStrings(record, pivot) >= 0){
+            i = i + 1;
+            swapInFile(fileDescriptor, i, j, lengthOfRecord);
+        }
+    }
+    swapInFile(fileDescriptor, i + 1, right, lengthOfRecord);
+    return i + 1;
+}
+
+//sorts file with system commands
+void sort(int fileDescriptor, int lengthOfRecord, int left, int right){
+    if (left < right){
+        int q = partition(fileDescriptor, lengthOfRecord, left, right);
+        sort(fileDescriptor, lengthOfRecord, left, q - 1);
+        sort(fileDescriptor, lengthOfRecord, q + 1, right);
+    }
+}
+
 
 int main(){
     generateWords(10, 4);
+    int fileDescriptor = open("dane.txt", O_RDWR);
+    system("cat dane.txt");
+    // // 0 -> equals, 1 -> string1 < string2, -1 -> string1 > string2
+    // printf("%d", compareStrings("AAAD", "BAAA"));
+    sort(fileDescriptor, 4, 0, 9);
+    close(fileDescriptor);
 }
