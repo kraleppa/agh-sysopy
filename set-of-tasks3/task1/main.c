51057c8ed50d7e387a20f28515d5ec8175ba96b5
Citoslav <misiek.nalepa@wp.pl>
2020-03-23 18:46:42 +0100

    first task done

---
+++ set-of-tasks3/task1/main.c
@@ -0,0 +1,45 @@
+#define _XOPEN_SOURCE 500
+
+#include <ftw.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+int display_info(const char *path, const struct stat *sb, int flagType, struct FTW *tw_buf){
+    char *absPath = (char*) calloc(150, sizeof(char));
+
+    realpath(path, absPath);
+    pid_t pid = fork();
+    if (pid == 0){
+        if (flagType == FTW_D){
+            printf("PID:       %d\n", getpid());
+            printf("Path:      %s\n", path);
+            char cmd[80] = "ls -l ";
+            strcat(cmd, path);
+            system(cmd);
+            printf("\n");
+        }
+        exit(0);
+    }
+    else{
+        wait(NULL);
+    }
+    free(absPath);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    char * path;
+    if (argc != 2)
+        path = ".";
+    else
+        path = argv[1];
+
+    nftw(path, display_info, 10, FTW_PHYS);
+
+    return 0;
+}
+
